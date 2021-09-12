be6197cc5c8cdc450039292359083e175cbb651f
Anh Dang Trung <dangtrunganh@gmail.com>
2021-09-12 21:07:15 +0700

    no message

--- examples/classification/perform_classification_with_linear_models.py
+++ examples/classification/perform_classification_with_linear_models.py
@@ -4,15 +4,19 @@ Created on Mon Aug 02 20:00:00 2021
 """
 import os
 from posixpath import join
+from sklearn.linear_model import LinearRegression
 import sys
 
 # sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
 
 from EvoCluster import EvoCluster
-from evoml.framework import EvoCC
+from evoml.framework import EvoCC, evocc
 from evoml.framework import datasets
 
 data_home = datasets.get_data_home(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../data')))
+
+# export EVOML_FRAMEWORK_DATA='../../data'
+# data_home = datasets.get_data_home()
 
 optimizer = ["SSA"]
 objective_func = ["SSE"]
@@ -35,17 +39,27 @@ evocluster = EvoCluster(
     labels_exist=False
 )
 
-datasets.split_dataset(os.path.join(data_home, "iris.csv"))
-input_of_evocluster = join(data_home, "iris")
-output_of_evocluster = join(data_home, "iris")
-
-evocluster.run(input_of_evocluster, output_of_evocluster)
+# divid dataset into train and test set
+# datasets.split_dataset(os.path.join(data_home, "iris.csv"))
 
 dataset_folder = join(data_home, "iris")
+output_of_evocluster = join(data_home, "iris")
 
-evocc = EvoCC(optimizer,
-              objective_func,
-              output_of_evocluster,
-              dataset_list,
-              dataset=dataset_folder)
+# run evocluster
+# evocluster.run(dataset_folder, output_of_evocluster)
+
+# run evocc
+evo_params = {'optimizer': optimizer,
+              'objective_func': objective_func,
+              'dataset_list': dataset_list}
+
+# Initialize our classifier
+clf = LinearRegression()
+
+evocc = EvoCC(evo_params=evo_params,
+              evo_folder=output_of_evocluster,
+              classifier=clf,
+              dataset=dataset_folder
+)
+
 evocc.run()
